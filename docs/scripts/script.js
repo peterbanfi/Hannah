2a4893066f5b401e117fbb98f641f3835e3407b0
Banfi Peter <p6t6rbanfi@gmail.com>
2018-08-14 23:09:32 +0200

    feat(header meta data:S): add meta data

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,1 +1,20 @@
-console.log('hi');
+'use strict';
+
+(function indexScope() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const btn = document.querySelector('.button');
+        const frameOne = document.querySelector('.frame-one');
+        const frameTwo = document.querySelector('.frame-two');
+
+        btn.addEventListener("mouseover", setEffect());
+        btn.addEventListener("onmouseout", resetEffect());
+
+        function setEffect() {
+            console.log('mouseover!');
+        }
+
+        function resetEffect() {
+            console.log('onmouseout!');
+        }
+    });
+}.bind({})());
